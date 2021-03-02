de9d1a3ce5545e84852b6b9ab7bff3e2fa7e0e2a
isdicely <75281465+isdicely@users.noreply.github.com>
2021-03-02 13:38:44 -0800

    Data Analysis View for Query_Question 5 Added

--- EmployeeSQL/Employee_Schema.sql
+++ EmployeeSQL/Employee_Schema.sql
@@ -93,7 +93,14 @@ SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 		JOIN departments d
 		ON (d.dept_no = de.dept_no);
 SELECT * FROM employee_department;
-		
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+CREATE VIEW Hercules_B_employees AS
+SELECT last_name, first_name
+FROM employees
+WHERE first_name = 'Hercules'
+	AND last_name LIKE 'B%';
+SELECT * FROM Hercules_B_employees;
 	
 
 
